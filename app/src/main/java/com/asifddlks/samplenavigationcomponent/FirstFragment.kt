0861d0c6511a9ac68e9c66ac6baa7c07ce21fbb7
Asif Ahmed <asif@techetronventures.com>
2022-03-22 12:49:23 +0600

    Initial commit

---
+++ app/src/main/java/com/asifddlks/samplenavigationcomponent/FirstFragment.kt
@@ -0,0 +1,35 @@
+package com.asifddlks.samplenavigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigation
+
+class FirstFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.textView).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.secondFragment)
+        }
+    }
+}
